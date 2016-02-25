ce3f9d551a1af1e59a5b1f7fde5053e2ffc3d561
rcasamajo <rcasamajo@iespoblenou.org>
2016-02-25 19:31:37 +0100

    Mostra tweets propis i llista de followings

--- js/controller.js
+++ js/controller.js
@@ -2,12 +2,14 @@
  * Created by rcasamajo on 18/2/16.
  */
 
-app.controller('mainController', ["$scope", "$firebaseArray", function($scope, $firebaseArray) {
+app.controller('mainController', ["$scope", "$firebaseArray", function($scope, $firebaseObject, $firebaseArray) {
+
+    $scope.userId = "rcc";
 
     // create a reference to the database location where we will store our data
-    var ref = new Firebase("https://ecaibtweet.firebaseio.com/users/");
+    var ref = new Firebase("https://ecaibtweet.firebaseio.com/users");
 
-    var query = ref.orderByChild("users").equalTo("rcc");
-    $scope.tweets = $firebaseArray(ref);
+    var query = ref.orderByKey().equalTo($scope.userId);
+    $scope.user = $firebaseObject(query);
 
 }]);
